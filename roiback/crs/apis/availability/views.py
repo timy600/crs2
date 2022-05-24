d2c03a0d39f0e5f812cc9b4d2a944769b9a6961e
Thibaut Chevée <thibaut.chevee@wpo.eu>
2022-05-24 23:38:24 +0200

    reorganize loops for availability view

--- roiback/crs/apis/availability/views.py
+++ roiback/crs/apis/availability/views.py
@@ -1,6 +1,8 @@
+import datetime
 from django.http import JsonResponse, HttpResponse
 from crs.models import Hotel, Room, Rate, Inventory
 from django.views.generic import TemplateView
+from crs.apis.utils import validate_date
 # from django.core.exceptions import ObjectDoesNotExist
 # from django.views.decorators.csrf import csrf_exempt
 
@@ -9,61 +11,42 @@ class AvailabilityView(TemplateView):
 
     def get(self, request, *args, **kwargs):
         code = kwargs["code"]
-        checkin_date = kwargs["checkin_date"]
-        checkout_date = kwargs["checkout_date"]
-        if checkout_date < checkin_date:
-            return HttpResponse('Error: checkout must come after checkin')
+        checkin_date = validate_date(kwargs["checkin_date"])
+        checkout_date = validate_date(kwargs["checkout_date"])
+        date_generated = [checkin_date + datetime.timedelta(days=x) for x in range(0, (checkout_date - checkin_date).days)]
         try:
             h = Hotel.objects.get(code=code)
         except Hotel.DoesNotExist:
             return HttpResponse('Exception: Data Not Found')
         rooms = Room.objects.filter(hotel=h)
-        rooms_data = []
+        rooms_dict = {}
         for room in rooms:
+            # rooms_dict.append()
+            rates_dict = {}
             rates = Rate.objects.filter(room=room)
-            rates_data = {}
-            room_dict = {}
-            room_dict[room.__str__()] = rates_data
             for rate in rates:
-                rate_dict = {
-                    "rate": rate.get_basic_data(),
-                    "inventories": [],
+                total_price = 0
+                price = rate.price
+                breakdown = {}
+                for d in date_generated:
+                    d = d.strftime("%Y-%m-%d")
+                    inventories = Inventory.objects.filter(date=d, rate=rate)
+                    for inv in inventories:
+                        if inv.availability is False:
+                            total_price = total_price + price
+                            breakdown[d] = {
+                                "price": price
+                            }
+                rates_dict[str(rate.code)] = {
+                    "total": total_price,
+                    "breakdown": [breakdown]
                 }
-                inventories = Inventory.objects.filter(rate=rate)
-                for i in inventories:
-                    rate_dict["inventories"].append(i.get_basic_data())
-                rates_data.append(rate_dict)
-
-            rooms_data.append(room_dict)
-
-        data = {
-            "rooms": rooms_data
-        }
-        # json = JSONRenderer().render(serialized_room.data)
-        # return json
-        return JsonResponse(data)
-"""
-    def get(self, request, *args, **kwargs):
-        code = kwargs["code"]
-        checkin_date = kwargs["checkin_date"]
-        checkout_date = kwargs["checkout_date"]
-        try:
-            h = Hotel.objects.get(code=code)
-        except Hotel.DoesNotExist:
-            return HttpResponse('Exception: Data Not Found')
-        rooms = Room.objects.filter(hotel=h)
-
-        rooms_data = []
-        total_price = 0
-        breakdown = {}
-        for inventory:
-            if
-            breakdown[date_focus] = {
-
+            rooms_dict[str(room.code)] = {
+                "rates": [rates_dict]
             }
+
         data = {
-            "total": total_price,
-            "breakdown": breakdown
+            "rooms": rooms_dict,
         }
+        print(data)
         return JsonResponse(data)
-"""
--- roiback/crs/apis/utils.py
+++ roiback/crs/apis/utils.py
@@ -1,11 +1,19 @@
 import datetime
 
 
+# def validate_date(date_text):
+#     try:
+#         datetime.datetime.strptime(date_text, '%Y-%m-%d')
+#     except ValueError:
+#         raise ValueError("Incorrect data format, should be YYYY-MM-DD")
+
+
 def validate_date(date_text):
     try:
-        datetime.datetime.strptime(date_text, '%Y-%m-%d')
+        response = datetime.datetime.strptime(date_text, '%Y-%m-%d')
     except ValueError:
         raise ValueError("Incorrect data format, should be YYYY-MM-DD")
+    return response
 
 
 def check_type(variable):
